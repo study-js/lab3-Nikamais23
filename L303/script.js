f6ef0d6a101dc74a69da37c09d61771e5496dceb
man.cho1826 <man.cho.7vili@gmail.com>
2017-11-18 15:19:12 +0400

    l304

--- L303/script.js
+++ L303/script.js
@@ -1,9 +1,11 @@
-var sheyvana= prompt("შეიყვანე რიცხვი");
+var price = prompt('ფასი: ');
 
-if(sheyvana.length <= 15){
-    alert(sheyvana);
-}
-else{
-    var s = sheyvana.substr(0, 15) + '...';
-    alert(s)
+var isUSD = price.includes("$");
+var isGEL = price.includes("L");
+
+
+if(isUSD){
+    document.write(parseInt(price.substr(1)) * 2.6);
+}else if(isGEL){
+    document.write(parseInt(price.substr(1)) / 2.6);
 }
